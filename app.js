a6ccd17ab8952c4617bde641f32cb21bc448d736
Wanglala <sue4100035045@gmail.com>
2020-06-30 23:31:42 +0800

    feat: confirm user password and unique email

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(port, () => {

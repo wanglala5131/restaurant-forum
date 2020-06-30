58d421842cb5e87def1c43f70dac9926799070c2
Wanglala <sue4100035045@gmail.com>
2020-07-01 01:20:04 +0800

    feat: Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,27 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
+  //驗證是否為已註冊的使用者
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  //驗證是否為管理員
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
   //前台
-  app.get('/', (req, res) => { res.redirect('/restaurants') })
+  app.get('/', authenticated, (req, res) => { res.redirect('/restaurants') })
   app.get('/restaurants', restController.getRestaurants)
 
   //後台
-  app.get('/admin', (req, res) => { res.redirect('/admin/restaurants') })
+  app.get('/admin', authenticatedAdmin, (req, res) => { res.redirect('/admin/restaurants') })
   app.get('/admin/restaurants', adminController.getRestaurants)
 
   //註冊

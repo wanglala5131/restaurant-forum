caed2185ad7848d78e9dcf90bf831460a9b5d413
Wanglala <sue4100035045@gmail.com>
2020-07-01 23:05:55 +0800

    feat: add edit page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -36,6 +36,11 @@ const adminController = {
         restaurant: restaurant
       })
     })
+  },
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
+      return res.render('admin/create', { restaurant: restaurant })
+    })
   }
 }
 

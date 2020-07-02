63d0d470a261cebb709a7213e38e4783963f205d
Wanglala <sue4100035045@gmail.com>
2020-07-03 00:46:05 +0800

    chore: ssl setting

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const db = require('./models') // 引入資料庫
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')

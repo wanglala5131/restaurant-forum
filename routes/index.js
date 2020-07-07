63dc5f4a813d45d0091da0f7ea85df6f3ae8ac62
Wanglala <sue4100035045@gmail.com>
2020-07-07 15:36:10 +0800

    feat: add category put feature

--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,8 @@ module.exports = (app, passport) => {
   app.put('/admin/users/:id', authenticatedAdmin, adminController.putUsers)
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
 
   //註冊
   app.get('/signup', userController.signUpPage)

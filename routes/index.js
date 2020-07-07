1a2eedde9f52767c3bdec850d8587cd173ec2742
Wanglala <sue4100035045@gmail.com>
2020-07-07 15:01:26 +0800

    feat: add categories page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
+const categoryController = require('../controllers/categoryController')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
@@ -35,6 +36,8 @@ module.exports = (app, passport) => {
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
   app.put('/admin/users/:id', authenticatedAdmin, adminController.putUsers)
+  app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+
   //註冊
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)

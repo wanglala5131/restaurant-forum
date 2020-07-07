6647c8b97ad8327c8a18b4dd3a4dbf918f7d0b06
Wanglala <sue4100035045@gmail.com>
2020-07-07 16:36:13 +0800

    feat: add category delete feature

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,7 @@ module.exports = (app, passport) => {
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
   app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
   //註冊
   app.get('/signup', userController.signUpPage)

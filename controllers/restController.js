748d37359eb398c8a9f811f8f6aeb3a521e27f3d
Wanglala <sue4100035045@gmail.com>
2020-07-09 01:21:00 +0800

    feat: add Comment model and assocoation among user,restaurants,comment

--- controllers/restController.js
+++ controllers/restController.js
@@ -19,13 +19,11 @@ const restController = {
       whereQuery['categoryId'] = categoryId
     }
     Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
-      // data for pagination
       let page = Number(req.query.page) || 1
       let pages = Math.ceil(result.count / pageLimit)
       let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
       let prev = page - 1 < 1 ? 1 : page - 1
       let next = page + 1 > pages ? pages : page + 1
-      // clean up restaurant data
       const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),

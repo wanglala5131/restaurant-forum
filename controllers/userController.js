bd2723d044038a52cf80c4f357736ed5141ae044
Wanglala <sue4100035045@gmail.com>
2020-07-09 18:18:23 +0800

    feat: user profile put feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const fs = require('fs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const userController = {
   signUpPage: (req, res) => {
@@ -52,6 +55,41 @@ const userController = {
       .then((user) => {
         return res.render('editUser')
       })
+  },
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "Please enter your name")
+      return res.redirect('back')
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: img.data.link
+            })
+              .then((user) => {
+                req.flash('success_messages', 'user was successfully to update')
+                res.redirect(`/users/${user.id}`)
+              })
+          })
+      })
+    }
+    else
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          })
+            .then((user) => {
+              req.flash('success_messages', 'user was successfully to update')
+              res.redirect(`/users/${user.id}`)
+            })
+        })
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,7 @@ module.exports = (app, passport) => {
 
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
+  app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
   app.post('/comments', authenticated, commentController.postComment)
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)

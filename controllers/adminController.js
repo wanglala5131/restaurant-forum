e1d1ccd11fa7de3a8004f3f9ceecbcec126cf6bf
Wanglala <sue4100035045@gmail.com>
2020-07-14 16:41:02 +0800

    feat: add adminService

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,10 +6,12 @@ const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
+const adminService = require('../services/adminService')
+
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true, nest: true, include: [Category] }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants: restaurants, user: req.user, isAuthenticated: req.isAuthenticated })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
     })
   },
 

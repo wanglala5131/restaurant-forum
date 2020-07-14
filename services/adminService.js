97eba6de5cd1d325ccdf754a40d1eb2a20b1848c
Wanglala <sue4100035045@gmail.com>
2020-07-14 23:23:48 +0800

    feat: add put api

--- services/adminService.js
+++ services/adminService.js
@@ -51,6 +51,49 @@ const adminService = {
       })
     }
   },
+  putRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ stauts: 'error', message: "name didn't exist" })
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.findByPk(req.params.id)
+          .then((restaurant) => {
+            restaurant.update({
+              name: req.body.name,
+              tel: req.body.tel,
+              address: req.body.address,
+              opening_hour: req.body.opening_hour,
+              description: req.body.description,
+              image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
+            })
+              .then((restaurant) => {
+                callback({ stauts: 'success', message: 'restaurant was successfully to update' })
+              })
+          })
+      })
+    }
+    else
+      return Restaurant.findByPk(req.params.id)
+        .then((restaurant) => {
+          restaurant.update({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hour: req.body.opening_hour,
+            description: req.body.description,
+            image: restaurant.image,
+            CategoryId: req.body.categoryId
+          })
+            .then((restaurant) => {
+              callback({ stauts: 'success', message: 'restaurant was successfully to update' })
+            })
+        })
+  },
   deleteRestaurant: (req, res, callback) => {
     return Restaurant.findByPk(req.params.id)
       .then((restaurant) => {
